b3c511aac9ebc4020db0695be2315628d811871c
Vladislav <vladikul98@gmail.com>
2023-10-12 02:23:55 +0300

    Version 2(use lombok fw)

--- src/main/java/com/laskin/springboot/pp_3_1_2/model/User.java
+++ src/main/java/com/laskin/springboot/pp_3_1_2/model/User.java
@@ -3,7 +3,7 @@ package com.laskin.springboot.pp_3_1_2.model;
 import jakarta.persistence.*;
 import lombok.Data;
 
-//@Data
+@Data
 @Entity
 @Table(name = "users")
 public class User {
@@ -25,64 +25,4 @@ public class User {
     @Column(name = "email")
     private String email;
 
-    public User() {
-    }
-
-    public User(String name, String surname, int age, String email) {
-        this.name = name;
-        this.surname = surname;
-        this.age = age;
-        this.email = email;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", surname='" + surname + '\'' +
-                ", age=" + age +
-                ", email='" + email + '\'' +
-                '}';
-    }
 }
